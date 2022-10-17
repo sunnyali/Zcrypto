9618bd5578ccb560b0cf77537e25a0c6a51392da
sunnyali <sunnyali45@gmail.com>
2022-10-17 12:21:31 +0400

    Regsiter page refernce from here
    https://www.youtube.com/watch?v=ajp8hmAKEhM&ab_channel=CodeWithAamir

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,7 +1,199 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { API_URL } from "@/config/index";
+import "antd/dist/antd.css";
+import { Form, Button, Checkbox, DatePicker, Input, Select, Space } from "antd";
 
 function index() {
-  return <div>Register</div>;
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const getCountries = async () => {
+      const res = await fetch(`${API_URL}/Miscellaneous/GetCountries`);
+      const { data } = await res.json();
+      setCountries(data);
+      console.log(data);
+    };
+    getCountries();
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Form
+          autoComplete="off"
+          labelCol={{ span: 10 }}
+          wrapperCol={{ span: 14 }}
+          onFinish={(values) => {
+            console.log({ values });
+          }}
+          onFinishFailed={(error) => {
+            console.log({ error });
+          }}
+        >
+          <Form.Item
+            name="firstName"
+            label="First Name"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your first Name",
+              },
+              { whitespace: true },
+              { min: 3 },
+            ]}
+            hasFeedback
+          >
+            <Input placeholder="Type your Firstname" />
+          </Form.Item>
+
+          <Form.Item
+            name="lastName"
+            label="Last Name"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your Last name",
+              },
+              { whitespace: true },
+              { min: 3 },
+            ]}
+            hasFeedback
+          >
+            <Input placeholder="Type your Last name" />
+          </Form.Item>
+
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your email",
+              },
+              { type: "email", message: "Please enter a valid email" },
+            ]}
+            hasFeedback
+          >
+            <Input placeholder="Type your email" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+              },
+              { min: 6 },
+              {
+                validator: (_, value) =>
+                  value && value.includes("A")
+                    ? Promise.resolve()
+                    : Promise.reject("Password does not match criteria."),
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password placeholder="Type your password" />
+          </Form.Item>
+
+          <Form.Item
+            name="confirmPassword"
+            label="Confirm Password"
+            dependencies={["password"]}
+            rules={[
+              {
+                required: true,
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    "The two passwords that you entered does not match."
+                  );
+                },
+              }),
+            ]}
+            hasFeedback
+          >
+            <Input.Password placeholder="Confirm your password" />
+          </Form.Item>
+
+          <Form.Item
+            name="country"
+            label="Country"
+            rules={[
+              {
+                required: true,
+                message: "Please select counrty",
+              },
+            ]}
+          >
+            <Select placeholder="Select Country">
+              {countries.map((country) => (
+                <Select.Option value={country.id}>{country.name}</Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+
+          {/* <Form.Item
+            name="dob"
+            label="Date of Birth"
+            rules={[
+              {
+                required: true,
+                message: "Please provide your date of birth",
+              },
+            ]}
+            hasFeedback
+          >
+            <DatePicker
+              style={{ width: "100%" }}
+              picker="date"
+              placeholder="Chose date of birth"
+            />
+          </Form.Item> */}
+
+          <Form.Item
+            name="mobileNo"
+            label="Mobile No"
+            rules={[
+              { type: "string", message: "Please enter a valid Mobile No" },
+            ]}
+          >
+            <Input placeholder="Add your Mobile No" />
+          </Form.Item>
+
+          {/* <Form.Item
+            name="agreement"
+            wrapperCol={{ span: 24 }}
+            valuePropName="checked"
+            rules={[
+              {
+                validator: (_, value) =>
+                  value
+                    ? Promise.resolve()
+                    : Promise.reject(
+                        "To proceed, you need to agree with our terms and conditions"
+                      ),
+              },
+            ]}
+          >
+            <Checkbox>
+              {" "}
+              Agree to our <a href="#">Terms and Conditions</a>
+            </Checkbox>
+          </Form.Item> */}
+
+          <Form.Item wrapperCol={{ span: 24 }}>
+            <Button block type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </header>
+    </div>
+  );
 }
 
 export default index;
